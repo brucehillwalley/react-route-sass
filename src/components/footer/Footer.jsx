7ad6e4c23568e70f1ff625fc676516a55f91f883
BruceHill <brucehillwalley@gmail.com>
2023-12-19 22:55:33 +0300

    finish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
+import { useLocation } from "react-router-dom"
 import "./Footer.scss"
 
 const Footer = () => {
+  const {pathname,state}=useLocation()
+console.log(state)
   return (
-    <footer className="horizontal">
+    <footer className={`${pathname ==="/"? "horizontal": pathname==="/contacts" ? "middle" : "vertical"}`}>
   <div className="social-icons">
     <a href="#">
       <i className="fab fa-twitter fa-2x" />
